3904aca459a012ae50154e58b2adeb3fc10f9a19
Brendon Lina <avgwest@outlook.com>
2023-08-07 20:36:19 -0300

    adicionando, JWT, LOGIN, LOGOUT ETC

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CidadeController;
 use App\Http\Controllers\MedicoController;
 use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +18,26 @@ use App\Http\Controllers\PacienteController;
 |
 */
 
+    
+    // Route::post('refresh', 'AuthController@refresh');
+    // Route::post('me', 'AuthController@me');
+
 Route::get('/', function(){
-    return "Seja Bem vindo Leia o README para entender o funcionamento da API";
+    return response()->json(['title' => 'Seja Bem vindo Leia o README para entender o funcionamento da API','api_version' => '1.0.0']);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/cidades', [CidadeController::class, 'index']);
 Route::post('/cidades', [CidadeController::class, 'store']);
-
-Route::get('/medicos', [MedicoController::class, 'index']);
-Route::post('/medicos', [MedicoController::class, 'store']);
-
-Route::get('/pacientes', [PacienteController::class, 'index']);
-Route::post('/pacientes', [PacienteController::class, 'store']);
-
 Route::get('/cidades/{id}/medicos', [CidadeController::class, 'cidadesMedicos']);
 
-
+Route::get('/medicos', [MedicoController::class, 'index']);
+Route::middleware('auth:api')->post('/medicos', [MedicoController::class, 'store']);
 Route::post('/medicos/{id}/pacientes', [MedicoController::class, 'PacientesMedicoStore']);
 Route::get('/medicos/{medico_id}/pacientes', [MedicoController::class, 'ListarPacientesMedico']);
 
-Route::put('/paciente/{id}', [PacienteController::class, 'update']);
+Route::middleware('auth:api')->get('/pacientes', [PacienteController::class, 'index']);
+Route::middleware('auth:api')->post('/pacientes', [PacienteController::class, 'store']);
+Route::middleware('auth:api')->put('/paciente/{id}', [PacienteController::class, 'update']);
